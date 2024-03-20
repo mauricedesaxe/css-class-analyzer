aa2b018fbb87cb60a8345d0d7e5e675bb30c671e
lazaralex98 <alex2012ro@gmail.com>
2024-03-20 04:03:57 +0200

    use net/html for parsing of html nodes
    not faster, but more standard and less
    chances of missing class attributes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module css-class-analyzer
 
 go 1.21.3
+
+require golang.org/x/net v0.22.0
--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"strings"
 	"time"
+
+	"golang.org/x/net/html"
 )
 
 func main() {
@@ -29,42 +31,33 @@ func classesFromFile(filename string) {
 		log.Fatalf("failed to open file: %s", err)
 	}
 	defer file.Close()
-	scanner := bufio.NewScanner(file)
+
+	// Parse the HTML file
+	doc, err := html.Parse(file)
+	if err != nil {
+		log.Fatalf("failed to parse HTML file: %s", err)
+	}
 
 	var globalClassNames []string
 
-	// Loop through all the lines
-	for scanner.Scan() {
-		// TODO we are assuming only HTML syntax.
-		// TODO we are assuming only double quotes for attribute values.
-		// TODO we are assuming only one class attribute per line.
-		// TODO we are assuming the class attribute is not split across multiple lines.
-
-		// Get the current line
-		line := scanner.Text()
-
-		// If the line does not contain a `class` attribute, skip it
-		if !strings.Contains(line, "class=") {
-			continue
+	// Define a recursive function to traverse the HTML nodes
+	var traverse func(*html.Node)
+	traverse = func(n *html.Node) {
+		if n.Type == html.ElementNode {
+			for _, a := range n.Attr {
+				if a.Key == "class" {
+					classNames := strings.Fields(a.Val)
+					globalClassNames = append(globalClassNames, classNames...)
+				}
+			}
 		}
-
-		// Find the start and end of the class attribute value in the line
-		start := strings.Index(line, "class=\"")
-		end := strings.Index(line[start+7:], "\"")
-
-		// get the string between `class="` and `"` or `class='` and `'`
-		// this is effectively the class names string
-		classString := line[start+7 : start+7+end]
-
-		// add the class names of this line to the class names of the whole file
-		localClassNames := strings.Split(classString, " ")
-		globalClassNames = append(globalClassNames, localClassNames...)
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			traverse(c)
+		}
 	}
 
-	// Check for errors during Scan. End of file is expected and not reported by Scan as an error.
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("error during file scan: %s", err)
-	}
+	// Start traversing from the root node
+	traverse(doc)
 
 	// remove duplicates from the class names of the whole file using a map cause it's faster
 	classMap := make(map[string]bool)
