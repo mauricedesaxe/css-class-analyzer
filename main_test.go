e59fc9e6e827bb6c25cbd8f054ef15fb2448bf11
lazaralex98 <alex2012ro@gmail.com>
2024-03-21 03:46:59 +0200

    add median duration

--- main_test.go
+++ main_test.go
@@ -9,13 +9,17 @@ import (
 func TestMainSpeed(t *testing.T) {
 	const runs = 100
 	var totalDuration time.Duration
+	var durations []time.Duration
 
 	for i := 0; i < runs; i++ {
 		startTime := time.Now()
 		main()
 		totalDuration += time.Since(startTime)
+		durations = append(durations, time.Since(startTime))
 	}
 
 	averageDuration := totalDuration / runs
+	medianDuration := durations[runs/2]
 	fmt.Printf("Average Duration over %d runs: %s\n", runs, averageDuration)
+	fmt.Printf("Median Duration over %d runs: %s\n", runs, medianDuration)
 }
