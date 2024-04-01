abf3f556888f519e42a69db2a70e7aa8b8324b00
lazaralex98 <alex2012ro@gmail.com>
2024-04-02 01:31:09 +0300

    improve simple test

--- analyzer/analyzer_test.go
+++ analyzer/analyzer_test.go
@@ -39,8 +39,8 @@ func TestSpeed(t *testing.T) {
 }
 
 func TestAccuracy(t *testing.T) {
-	inputHTML := `<div class="bg-slate-950 text-gray-100 max-w-4xl mx-auto rounded-lg shadow-lg"><form class="flex flex-col gap-4"><div class="bg-red-500 p-2 rounded-md">Invalid HTML. Please enter valid HTML content.</div><textarea placeholder="Paste your HTML here" class="bg-slate-800 border border-slate-700 placeholder-gray-400 text-white rounded-md p-2">dialog</textarea><button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-500 disabled:cursor-not-allowed">Analyze</button></form><div class="flex flex-col gap-2 pt-6"></div></div>`
-	expectedClasses := []string{"bg-slate-950", "text-gray-100", "max-w-4xl", "mx-auto", "rounded-lg", "shadow-lg", "flex", "gap-4", "bg-red-500", "p-2", "rounded-md", "bg-slate-800", "border", "border-slate-700", "placeholder-gray-400", "text-white", "p-2", "bg-blue-500", "hover:bg-blue-700", "text-white", "font-bold", "py-2", "px-4", "rounded", "disabled:bg-gray-500", "disabled:cursor-not-allowed", "flex", "gap-2", "pt-6"}
+	sampleHTML := `<div class="bg-slate-950 text-gray-100 max-w-4xl mx-auto rounded-lg shadow-lg"><form class="flex flex-col gap-4"><div class="bg-red-500 p-2 rounded-md">Invalid HTML. Please enter valid HTML content.</div><textarea placeholder="Paste your HTML here" class="bg-slate-800 border border-slate-700 placeholder-gray-400 text-white rounded-md p-2">dialog</textarea><button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-gray-500 disabled:cursor-not-allowed">Analyze</button></form><div class="flex flex-col gap-2 pt-6"></div></div>`
+	expectedClasses := []string{"bg-slate-950", "text-gray-100", "max-w-4xl", "mx-auto", "rounded-lg", "shadow-lg", "flex", "flex-col", "gap-4", "bg-red-500", "p-2", "rounded-md", "bg-slate-800", "border", "border-slate-700", "placeholder-gray-400", "text-white", "bg-blue-500", "hover:bg-blue-700", "text-white", "font-bold", "py-2", "px-4", "rounded", "disabled:bg-gray-500", "disabled:cursor-not-allowed", "gap-2", "pt-6"}
 
 	// Create a temporary file
 	tmpFile, err := os.CreateTemp("", "test.html")
@@ -50,30 +50,55 @@ func TestAccuracy(t *testing.T) {
 	defer os.Remove(tmpFile.Name())
 
 	// Write the input HTML to the temporary file
-	_, err = tmpFile.WriteString(inputHTML)
+	_, err = tmpFile.WriteString(sampleHTML)
 	if err != nil {
 		t.Fatalf("failed to write to temp file: %s", err)
 	}
 	tmpFile.Close()
 
 	// Extract classes from the temporary file
-	classNames, err := classesFromFile(tmpFile.Name())
+	receivedClasses, err := classesFromFile(tmpFile.Name())
 	if err != nil {
 		t.Fatalf("failed to extract classes: %s", err)
 	}
 
-	// Compare the extracted classes with the expected classes
-	// if there's a difference, print the difference
-	for _, expectedClass := range expectedClasses {
-		found := false
-		for _, className := range classNames {
-			if className == expectedClass {
-				found = true
-				break
-			}
-		}
-		if !found {
-			t.Errorf("expected class %s not found", expectedClass)
+	// check that the length of `receivedClasses` is equal to the length of `expectedClasses`
+	if len(receivedClasses) != len(expectedClasses) {
+		t.Errorf("Expected %d classes, got %d", len(expectedClasses), len(receivedClasses))
+	}
+
+	// check that the classes in `expectedClasses` are present in the `receivedClasses`
+	var expectedClassesThatWerentFound []string
+	for _, className := range expectedClasses {
+		if !contains(receivedClasses, className) {
+			expectedClassesThatWerentFound = append(expectedClassesThatWerentFound, className)
 		}
 	}
+	if len(expectedClassesThatWerentFound) > 0 {
+		t.Errorf("Expected classes that weren't found: %s", expectedClassesThatWerentFound)
+	}
+
+	// check that there are no classes in `receivedClasses` that are not in `expectedClasses`
+	var unexpectedClassesFound []string
+	for _, className := range receivedClasses {
+		if !contains(expectedClasses, className) {
+			unexpectedClassesFound = append(unexpectedClassesFound, className)
+		}
+	}
+	if len(unexpectedClassesFound) > 0 {
+		t.Errorf("Unexpected classes found: %s", unexpectedClassesFound)
+	}
+
+	// log some information
+	t.Logf("Expected classes that weren't found: %d", len(expectedClassesThatWerentFound))
+	t.Logf("Unexpected classes found: %d", len(unexpectedClassesFound))
+}
+
+func contains(classes []string, class string) bool {
+	for _, c := range classes {
+		if c == class {
+			return true
+		}
+	}
+	return false
 }
